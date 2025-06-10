3953a45e3ca4afd14480a6ce23a8c39c7c0d22fc
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2025-06-10 14:24:48 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,68 +1,37 @@
 import flwr as fl
 import tensorflow as tf
-import numpy as np
 import os
-import json
+from model import create_model
+from loader import load_dataset
 
+# 環境變數指定 client ID，例如：CLIENT_ID=client_1 python3 client.py
 CLIENT_ID = os.environ.get("CLIENT_ID", "client_1")
-IMG_DIR = "JPEGImages"
-ANNOTATION_DIR = "Annotations"
-IMG_SIZE = (224, 224)
-BATCH_SIZE = 16
-NUM_CLASSES = 9
-train_list_path = f"{CLIENT_ID}.txt"
 
-def load_data(file_path):
-    with open(file_path) as f:
-        filenames = [line.strip() for line in f.readlines()]
+# 目錄設定
+IMG_DIR = "./JPEGImages"
+JSON_DIR = "./Annotations"
+SPLIT_FILE = f"./ImageSets/{CLIENT_ID}.txt"  # e.g., ./ImageSets/client_1.txt
 
-    images, labels = [], []
-    for name in filenames:
-        img_path = os.path.join(IMG_DIR, f"{name}.jpg")
-        json_path = os.path.join(ANNOTATION_DIR, f"{name}.json")
-        if not os.path.exists(img_path) or not os.path.exists(json_path):
-            continue
-        img = tf.keras.utils.load_img(img_path, target_size=IMG_SIZE)
-        img = tf.keras.utils.img_to_array(img) / 255.0
-        with open(json_path) as jf:
-            label = json.load(jf).get("action")
-        if label is None:
-            continue
-        images.append(img)
-        labels.append(label)
-    label_to_index = {name: i for i, name in enumerate(sorted(set(labels)))}
-    int_labels = [label_to_index[l] for l in labels]
-    y = tf.keras.utils.to_categorical(int_labels, num_classes=NUM_CLASSES)
-    return tf.data.Dataset.from_tensor_slices((np.array(images), y)).shuffle(1000).batch(BATCH_SIZE)
+# 載入資料
+train_ds = load_dataset(SPLIT_FILE, IMG_DIR, JSON_DIR)
 
-train_ds = load_data(train_list_path)
+# 建立模型
+model = create_model()
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(224, 224, 3)),
-    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
-])
-
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
+# 封裝成 Flower Client
 class FlowerClient(fl.client.NumPyClient):
     def get_parameters(self, config):
         return model.get_weights()
 
-    def set_parameters(self, parameters):
-        model.set_weights(parameters)
-
     def fit(self, parameters, config):
-        self.set_parameters(parameters)
+        model.set_weights(parameters)
         model.fit(train_ds, epochs=1)
-        return self.get_parameters(config), len(train_ds), {}
+        return model.get_weights(), len(train_ds), {}
 
     def evaluate(self, parameters, config):
-        return 0.0, 0, {}
+        model.set_weights(parameters)
+        loss, acc = model.evaluate(train_ds)
+        return loss, len(train_ds), {"accuracy": acc}
 
+# 啟動 client
 fl.client.start_numpy_client(server_address="192.168.183.128:8080", client=FlowerClient())
